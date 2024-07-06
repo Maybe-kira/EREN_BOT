a4ecfe79639d159985b31f161b74230df3dd88bf
JstKira <jsalotaibi5@gmail.com>
2024-07-06 23:39:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('✅ㅤIniciando...');
-import {join, dirname} from 'path';
+import path from 'path';
 import {createRequire} from 'module';
 import {fileURLToPath} from 'url';
 import {setupMaster, fork} from 'cluster';
@@ -74,38 +74,43 @@ function start(file) {
     }
   }
 }
-import fs from 'fs';
-import firebaseAdmin from 'firebase-admin';
+// GET DATABASE >>
+async function fetchDataAndSave() {
+    try {
+        try {
+            fs.unlinkSync('database.json');
+            console.log('database.json file deleted successfully.');
+        } catch (err) {
+            console.error('Error deleting database.json file:', err);
+        }
 
-try {
-    fs.unlinkSync('database.json');
-    console.log('database.json file deleted successfully.');
-} catch (err) {
-    console.error('Error deleting database.json file:', err);
+        const serviceAccount = JSON.parse(fs.readFileSync('./firebase-key.json', 'utf8'));
+        const id = serviceAccount.project_id
+        firebaseAdmin.initializeApp({
+            credential: firebaseAdmin.credential.cert(serviceAccount),
+            databaseURL: `https://${id}-default-rtdb.firebaseio.com`
+        });
+
+        const dbRef = firebaseAdmin.database().ref('/');
+        const snapshot = await dbRef.once('value');
+        const data = snapshot.val();
+
+        const replacedData = replaceInvalidKeys(data);
+
+        fs.writeFileSync('database.json', JSON.stringify(replacedData, null, 4), 'utf8');
+        console.log('Data saved to database.json file successfully.');
+
+        start('main.js');
+    } catch (error) {
+        console.error('Error fetching data or saving to database.json:', error);
+    }
 }
-
-const serviceAccount = JSON.parse(fs.readFileSync('./firebase-key.json', 'utf8')); 
-const id = serviceAccount.project_id
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-    databaseURL: `https://${id}-default-rtdb.firebaseio.com`
-});
-
-const dbRef = firebaseAdmin.database().ref('/');
-dbRef.once('value', (snapshot) => {
-    const data = snapshot.val();
-
-    const replacedData = replaceInvalidKeys(data);
-
-    fs.writeFileSync('database.json', JSON.stringify(replacedData, null, 4), 'utf8');
-    console.log('Data saved to database.json file successfully.');
-});
 
 function replaceInvalidKeys(obj) {
     const newObj = {};
     for (const key in obj) {
         if (Object.hasOwnProperty.call(obj, key)) {
-            const newKey = key.replace(/,/g, '.');
+            const newKey = key.replace(/,/g, '.'); // استبدال ',' ب'.'
             newObj[newKey] = obj[key];
             if (typeof obj[key] === 'object') {
                 newObj[newKey] = replaceInvalidKeys(obj[key]);
@@ -115,8 +120,4 @@ function replaceInvalidKeys(obj) {
     return newObj;
 }
 
-
-setTimeout(() => {
-    console.log('The next codes are executed after a delay of 26 seconds...');
-    start('main.js')
-}, 26000);
+fetchDataAndSave();
