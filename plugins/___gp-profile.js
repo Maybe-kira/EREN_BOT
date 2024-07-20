379aa0a9022dc5de3f62da30cd417fb4262aeec4
Maybe-kira <jadeksa599@gmail.com>
2024-07-20 16:43:18 +0300

    Update ___gp-profile.js

--- plugins/___gp-profile.js
+++ plugins/___gp-profile.js
@@ -1,24 +1,24 @@
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { createHash } from 'crypto';
+import PhoneNumber from 'awesome-phonenumber';
+import { canLevelUp, xpRange } from '../lib/levelling.js';
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`;
 
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-let user = global.db.data.users[who]
-let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-let { min, xp, max } = xpRange(user.level, global.multiplier)
-let username = conn.getName(who)
-let math = max - xp
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-let maxwarn = 3; // تعريف maxwarn بقيمة افتراضية
-let ttms = `${userq.xp}` / 8;
-const userq = await Levels.fetch(citel.sender, "RandomXP");
-let str = `*❖ ── ✦ ──『⚜️』── ✦ ── ❖*
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png');
+    let user = global.db.data.users[who];
+    let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = user;
+    let { min, xp, max } = xpRange(user.level, global.multiplier);
+    let username = conn.getName(who);
+    let math = max - xp;
+    let prem = global.prems.includes(who.split`@`[0]);
+    let sn = createHash('md5').update(who).digest('hex');
+    let maxwarn = 3; // تعريف maxwarn بقيمة افتراضية
+    let userq = await Levels.fetch(who, "RandomXP");
+    let ttms = `${userq.xp}` / 8;
+    let str = `*❖ ── ✦ ──『⚜️』── ✦ ── ❖*
 *⤶❏ الاسم 👤:* ${username} ${registered ? '\n   • ' + name + ' ' : ''}   
 *⤶❏ المنشن 📧 : @${who.replace(/@.+/, '')}*
 *⤶❏ الرقم ☎️ : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}*
@@ -32,11 +32,11 @@ let str = `*❖ ── ✦ ──『⚜️』── ✦ ── ❖*
 *⤶❏ التسجيل 📄 : ${registered ? 'يب' : 'لا'}*
 *❖ ── ✦ ──『⚜️』── ✦ ── ❖*`;
 
-    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] });
+};
 
-}
-handler.help = ['perfil']
-handler.tags = ['group']
-handler.command = ['بروفايل', 'perfil', 'ماي'] 
+handler.help = ['perfil'];
+handler.tags = ['group'];
+handler.command = ['بروفايل', 'perfil', 'ماي'];
 
-export default handler
+export default handler;
